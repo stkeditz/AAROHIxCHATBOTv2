459ab1338f59cf2407152c671c12c396b9996c53
sᴀɢᴀʀˣᴀᴅɪᴛɪ <112709751+stkeditz@users.noreply.github.com>
2024-01-22 17:27:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 27353035))
-API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
+API_ID = "14050586"
+API_HASH = "42a60d9c657b106370c79bb0a8ac560c"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", "5465943450"))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "LOVE_FEELINGS_WILL1")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "alonegroup121")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "dil_sagar_121")
+OWNER_ID = "5465943450"
+SUPPORT_GRP = "LOVE_FEELINGS_WILL1"
+UPDATE_CHNL = "alonegroup121"
+OWNER_USERNAME = "dil_sagar_121"
 
