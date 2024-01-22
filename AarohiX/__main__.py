cb73f0c802a59b8fccb67400fc49af4e653564f7
sᴀɢᴀʀˣᴀᴅɪᴛɪ <112709751+stkeditz@users.noreply.github.com>
2024-01-22 03:25:45 -0800

    Add files via upload

---
+++ AarohiX/__main__.py
@@ -0,0 +1,26 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+
+from AarohiX import LOGGER, AarohiX
+from AarohiX.modules import ALL_MODULES
+
+
+async def anony_boot():
+    try:
+        await AarohiX.start()
+    except Exception as ex:
+        LOGGER.error(ex)
+        quit(1)
+
+    for all_module in ALL_MODULES:
+        importlib.import_module("AarohiX.modules." + all_module)
+
+    LOGGER.info(f"@{AarohiX.username} Started.")
+    await idle()
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(anony_boot())
+    LOGGER.info("Stopping AarohiX Bot...")
