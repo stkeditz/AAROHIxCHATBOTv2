7c2771c5d7b213e4b1977deced83c2c9d6efc414
sᴀɢᴀʀ ᴛɪᴡᴀʀɪ <112709751+stkeditz@users.noreply.github.com>
2024-12-07 00:01:11 +0530

    Update reaction.py

--- AarohiX/modules/reaction.py
+++ AarohiX/modules/reaction.py
@@ -1,18 +1,10 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from pyrogram.errors import MessageIdInvalid, ChatAdminRequired, EmoticonInvalid, ReactionInvalid, RPCError
-from random import choice
-from config import EMOJIS
 from AarohiX import AarohiX
 
-
-@AarohiX.on_message(filters.all)
-async def send_reaction(_, msg: Message):
+@AarohiX.on_message(filters.incoming)
+async def react_to_messages(client: Client, message: Message):
     try:
-
-        if not msg.from_user.is_bot:
-            await msg.react(choice(EMOJIS))
-    except (MessageIdInvalid, EmoticonInvalid, ChatAdminRequired, ReactionInvalid, RPCError) as e:
-    
-        print(f"Reaction failed: {e}")
-        pass
+        await message.react("😒")
+    except Exception as e:
+        print(f"Failed to react to message: {e}")
